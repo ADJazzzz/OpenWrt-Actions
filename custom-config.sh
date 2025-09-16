c2c9c6de223042de9b18cff45fcd9c5b55d0a740
ADJazz <68748165+ADJazzzz@users.noreply.github.com>
2025-09-16 13:03:51 +0800

    Update custom-config.sh

--- custom-config.sh
+++
@@ -1,7 +0,0 @@
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-
-sed -i "s/OpenWrt /ADJazz compiled $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-
-[ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
-    echo 'CONFIG_KERNEL_BUILD_USER="ADJazz"' >>.config ||
-    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"ADJazz"@' .config
