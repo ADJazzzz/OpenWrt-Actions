0cbac7e891e5ec6caaae10004f2da18db1bd688f
ADJazz <68748165+ADJazzzz@users.noreply.github.com>
2026-04-04 15:35:47 +0800

    Update custom-config.sh

--- custom-config.sh
+++ custom-config.sh
@@ -1,5 +1,7 @@
 sed -i "s/LEDE /ADJazz compiled $(TZ=UTC-8 date "+%Y.%m.%d") @ LEDE /g" package/lean/default-settings/files/zzz-default-settings
 
-[ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
-    echo 'CONFIG_KERNEL_BUILD_USER="ADJazz"' >>.config ||
-    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"ADJazz"@' .config
+if grep -q '^CONFIG_KERNEL_BUILD_USER=' .config; then
+    sed -i 's@^CONFIG_KERNEL_BUILD_USER=.*@CONFIG_KERNEL_BUILD_USER="ADJazz"@' .config
+else
+    echo 'CONFIG_KERNEL_BUILD_USER="ADJazz"' >> .config
+fi
